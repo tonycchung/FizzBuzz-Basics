b10ecad6614883808349508e4cee88a3ef3abbd0
Tony Chung <anthonyuchung@gmail.com>
2014-05-05 15:48:29 -0700

    First commit

---
+++ FizzBuzz.rb
@@ -0,0 +1,17 @@
+def FizzBuzz(x)
+	if x == 0
+		puts x
+	elsif x % 15 == 0
+		puts "FizzBuzz"
+	elsif x % 3 == 0
+		puts "Fizz"
+	elsif x % 5 == 0
+		puts "Buzz"
+	else
+		puts x
+	end
+end
+
+puts "Please enter a number:"
+number = gets
+FizzBuzz(Integer(number))
